881218b0e67a62f1dfd11722e9694ab55a38af80
laxmanverma90 <laxman.verma90@gmail.com>
2019-10-14 08:34:34 +0530

    Created user dto

--- src/main/java/com/hcl/security/controller/UserController.java
+++ src/main/java/com/hcl/security/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.hcl.security.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
 public class UserController {
 
 }
--- src/main/java/com/hcl/security/entity/User.java
+++ src/main/java/com/hcl/security/entity/User.java
@@ -1,5 +1,14 @@
 package com.hcl.security.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
 
+	private String name;
+	private String email;
+	private String phone;
+	private String city;
 }
